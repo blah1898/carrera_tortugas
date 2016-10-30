a51c85fb77f3ad5bed0c09e3f0fe2be6523aba50
Bruce Hernandez <@blah1898>
2016-10-30 12:27:56 -0700

    Definir apropiadamente las constantes de símbolos

--- src/generate_unicode.py
+++ src/generate_unicode.py
@@ -5,7 +5,8 @@ HEADER =\
 #define _XOPEN_SOURCE_EXTENDED 1
 #include <ncurses.h>
 #ifndef _SIMBOLOS_H
-#define _SIMBOLOS_H'''
+#define _SIMBOLOS_H
+'''
 
 FOOTER=\
 '''
@@ -16,7 +17,8 @@ if __name__ == '__main__':
     header_symbols = open ('simbolos.h', 'w')
     header_symbols.write(HEADER) 
     for codepoint in range(0x2500, 0x259F):
-        header_symbols.write("const cchar_t U{0:X} = {{0, L'{1:s}'}};\n".format(codepoint, chr(codepoint)))
-        print("const cchar_t U{0:X} = {{0, L'{1:s}'}};".format(codepoint, chr(codepoint)))
+        header_symbols.write("static const cchar_t U{0:X} = {{0, {{L'{1:s}', L'\\0'}}, 0}};\n".format(codepoint, chr(codepoint)))
+
+    header_symbols.write("static const cchar_t U{0:X} = {{0, {{L'{1:s}', L'\\0'}}, 0}};\n".format(0x1F422, chr(0x1F422)))
     header_symbols.write(FOOTER) 
     header_symbols.close()
